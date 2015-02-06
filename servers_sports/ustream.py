c553b482e524ec859d888acf63fb347092881dbd
pipcat <pip@pipcat.com>
2015-02-06 21:23:01 +0100

    Update ustream.py

--- servers_sports/ustream.py
+++ servers_sports/ustream.py
@@ -19,14 +19,19 @@ def find_url_play(data, headers):
 
     '''
 <iframe width="700" height="450" src="http://www.ustream.tv/embed/19903720?v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;"> </iframe>
+
+<iframe src="//www.ustream.tv/embed/19928114?wmode=direct" style="border: 0 none transparent;" frameborder="no" width="730" height="460"></iframe>
     '''
 
+    data = data.replace('src="//', 'src="http://')
     cid = scrapertools.find_single_match (data, 'src="http://www.ustream.tv/embed/([^\?]+)')
     if cid == '':
         #cid = scrapertools.find_single_match (data, "ustream.vars.contentId=['\"]([^'\"]+)")
         cid = scrapertools.find_single_match (data, "ustream.vars.channelId=['\"]([^'\"]+)")        
         if cid == '':
-            return ''
+            cid = scrapertools.find_single_match (data, "ustream.vars.ctxtId=['\"]([^'\"]+)")        
+            if cid == '':
+                return ''
 
     url = 'http://iphone-streaming.ustream.tv/uhls/%s/streams/live/iphone/playlist.m3u8' % cid
 
